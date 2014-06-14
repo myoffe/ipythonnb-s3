9064458b445b1fd60801d552dd6a58870cee2ee6
Michael Yoffe <myoffe@gmail.com>
2014-06-15 00:03:38 +0300

    wip

--- s3nbmanager.py
+++ s3nbmanager.py
@@ -14,6 +14,7 @@ class S3NotebookManager(NotebookManager):
     aws_secret_access_key = Unicode(config=True, help='AWS secret access key.')
     s3_bucket = Unicode('', config=True, help='Bucket name for notebooks.')
     s3_prefix = Unicode('', config=True, help='Key prefix of notebook location')
+    notebook_dir = s3_prefix
 
     def __init__(self, **kwargs):
         super(S3NotebookManager, self).__init__(**kwargs)
@@ -24,8 +25,98 @@ class S3NotebookManager(NotebookManager):
         self.s3_con = boto.connect_s3(access_key, secret_key)
         self.bucket = self.s3_con.get_bucket(self.s3_bucket)
 
-    def load_notebook_names(self):
         self.mapping = {}
+        self.load_notebook_names()
+
+    def path_exists(self, path):
+        return self.notebook_exists(path)
+
+    def is_hidden(self, path):
+        return False
+
+    def notebook_exists(self, name, path=''):
+        key = self.bucket.get_key(self.s3_prefix + name)
+        return key is not None
+
+    # TODO: Remove this after we create the contents web service and directories are
+    # no longer listed by the notebook web service.
+    def list_dirs(self, path):
+        """List the directory models for a given API style path."""
+        return []
+
+    # TODO: Remove this after we create the contents web service and directories are
+    # no longer listed by the notebook web service.
+    def get_dir_model(self, name, path=''):
+        """Get the directory model given a directory name and its API style path.
+
+        The keys in the model should be:
+        * name
+        * path
+        * last_modified
+        * created
+        * type='directory'
+        """
+        raise NotImplementedError('must be implemented in a subclass')
+
+    def list_notebooks(self, path=''):
+        models = []
+        for key in self.bucket.list(self.s3_prefix):
+            models.append(dict(name=key.name, path='', last_modifie=key.last_modified,
+                               created=key.last_modified, type='notebook'))
+        return models
+
+    def get_notebook(self, name, path='', content=True):
+        """Get the notebook model with or without content."""
+        key = self.bucket.get_key(self.s3_prefix + name)
+        last_modified = key.last_modified
+        model = dict(name=name, path=name, last_modified=last_modified, created=last_modified, type='notebook')
+        if content:
+            model['content'] = key.get_contents_as_string()
+        return model
+
+    def save_notebook(self, model, name, path=''):
+        """Save the notebook and return the model with no content."""
+        nb = current.to_notebook_json(model['content'])
+        # self.check_and_sign(nb, name, path)
+        key = self.bucket.new_key(self.s3_prefix + name)
+        key.set_contents_from_string(nb)
+        return self.get_notebook(name, name, content=False)
+
+    def update_notebook(self, model, name, path=''):
+        """Update the notebook and return the model with no content."""
+        new_name = model.get('name', name)
+        if name != new_name:
+            key = self.bucket.get_key(name)
+            key.name = new_name
+        return self.get_notebook(new_name)
+
+    def delete_notebook(self, name, path=''):
+        """Delete notebook by name and path."""
+        self.bucket.delete_key(name)
+
+    def create_checkpoint(self, name, path=''):
+        """Create a checkpoint of the current state of a notebook
+
+        Returns a checkpoint_id for the new checkpoint.
+        """
+        raise NotImplementedError("must be implemented in a subclass")
+
+    def list_checkpoints(self, name, path=''):
+        """Return a list of checkpoints for a given notebook"""
+        return []
+
+    def restore_checkpoint(self, checkpoint_id, name, path=''):
+        """Restore a notebook from one of its checkpoints"""
+        raise NotImplementedError("must be implemented in a subclass")
+
+    def delete_checkpoint(self, checkpoint_id, name, path=''):
+        """delete a checkpoint for a notebook"""
+        raise NotImplementedError("must be implemented in a subclass")
+
+    def info_string(self):
+        return "Serving notebooks"
+
+    def load_notebook_names(self):
         keys = self.bucket.list(self.s3_prefix)
         ids = [k.name.split('/')[-1] for k in keys]
 
@@ -33,10 +124,10 @@ class S3NotebookManager(NotebookManager):
             name = self.bucket.get_key(self.s3_prefix + id).get_metadata('nbname')
             self.mapping[id] = name
 
-    def list_notebooks(self):
-        data = [dict(notebook_id=id, name=name) for id, name in self.mapping.items()]
-        data = sorted(data, key=lambda item: item['name'])
-        return data
+    # def list_notebooks(self):
+    #     data = [dict(notebook_id=id, name=name) for id, name in self.mapping.items()]
+    #     data = sorted(data, key=lambda item: item['name'])
+    #     return data
 
     def read_notebook_object(self, notebook_id):
         if not self.notebook_exists(notebook_id):
